ed6c48e54d48a10596e59e21c5e2b07092ceff46
swrangsar basumatary <swrangsar@iitb.ac.in>
2014-06-24 15:51:41 +0530

    commit at 20140624155141

--- what_the_if.erl
+++ what_the_if.erl
@@ -1,5 +1,6 @@
 -module(what_the_if).
--export([heh_fine/0, oh_god/1, help_me/1]).
+-compile(export_all).
+
 
 heh_fine() ->
     if 1 =:= 1 ->
@@ -11,17 +12,41 @@ heh_fine() ->
     if 1 =:= 2, 1 =:= 1 ->
         fails
     end.
-    
+
+
 oh_god(N) ->
     if N =:= 2 -> might_succeed;
         true -> always_does %% this is Erlang's if's 'else!'
     end.
 
+
 help_me(Animal) ->
-    Talk = if Animal == cat  -> "meow";
-                Animal == beef -> "mooo";
-                Animal == dog -> "bark";
-                Animal == tree -> "bark";
+    Talk = if   Animal == cat     -> "meow";
+                Animal == beef    -> "mooo";
+                Animal == dog     -> "bark";
+                Animal == tree    -> "bark";
                 true -> "fgdadfgna"
-            end,
+           end,
     {Animal, "says " ++ Talk ++ "!"}.
+
+
+insert(X, []) ->
+    [X];
+insert(X,Set) ->
+    case lists:member(X,Set) of
+        true    -> Set;
+        false   -> [X|Set]
+    end.
+
+
+beach(Temperature) ->
+    case Temperature of
+        {celsius, N} when N >= 20, N =< 45 ->
+            'favorable';
+        {kelvin, N} when N >= 293, N =< 318 ->
+            'scientifically favorable';
+        {fahrenheit, N} when N >= 68, N =< 113 ->
+            'favorable in the US';
+        _ ->
+            'avoid beach'
+    end.
