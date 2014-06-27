0e542b61732e4957683faf35df6a5ebb4c9fad5f
Chaitanya <kartiksudarshan@kartiksudarshan-desktop.spann.local>
2014-06-27 19:06:10 +0530

    commit at 20140627190610

--- hhfuns.erl
+++ hhfuns.erl
@@ -34,3 +34,36 @@ b(F) ->
 base() ->
     A = 1,
     (fun(A) -> A = 2 end)(2).
+
+
+%% only keep even numbers
+even(L) -> lists:reverse(even(L,[])).
+
+even([], Acc) -> Acc;
+even([H|T], Acc) when H rem 2 == 0 ->
+    even(T, [H|Acc]);
+even([_|T]), Acc) ->
+    even(T, Acc).
+
+
+%% only keep men older than 60
+old_men(L) -> lists:reverse(old_men(L,[])).
+
+old_men([], Acc) -> Acc;
+old_men([Person = {male, Age}|People], Acc) when Age > 60 ->
+    old_men(People, [Person|Acc]);
+old_men([_|People], Acc) ->
+    old_men(People, Acc).
+
+
+filter(Pred, L) -> lists:reverse(filter(Pred, L, [])).
+
+filter(_, [], Acc) -> Acc;
+filter(Pred, [H|T], Acc) ->
+    case Pred(H) of
+        true    -> filter(Pred, T, [H|Acc]);
+        false   -> filter(Pred, T, Acc)
+    end.
+
+
+
