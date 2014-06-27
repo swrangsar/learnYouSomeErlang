ec32b76ed0d341204592477495cbe4b1876c06ac
Chaitanya <kartiksudarshan@kartiksudarshan-desktop.spann.local>
2014-06-27 19:52:07 +0530

    commit at 20140627195207

--- hhfuns.erl
+++ hhfuns.erl
@@ -42,7 +42,7 @@ even(L) -> lists:reverse(even(L,[])).
 even([], Acc) -> Acc;
 even([H|T], Acc) when H rem 2 == 0 ->
     even(T, [H|Acc]);
-even([_|T]), Acc) ->
+even([_|T], Acc) ->
     even(T, Acc).
 
 
@@ -65,5 +65,49 @@ filter(Pred, [H|T], Acc) ->
         false   -> filter(Pred, T, Acc)
     end.
 
+
+%% find the maximum of a list
+max([H|T]) -> max2(T, H).
+
+max2([], Max) -> Max;
+max2([H|T], Max) when H > Max -> max2(T,H);
+max2([_|T], Max) -> max2(T, Max).
+
+
+%% find the minimum of a list
+min([H|T]) -> min2(T,H).
+
+min2([], Min) -> Min;
+min2([H|T], Min) when H < Min -> min2(T, H);
+min2([_|T], Min) -> min2(T, Min).
+
+
+%% sum of all the elements of a list
+sum(L) -> sum(L, 0).
+
+sum([], Sum) -> Sum;
+sum([H|T], Sum) -> sum(T, H+Sum).
+
+
+fold(_,Start,[]) -> Start;
+fold(F,Start,[H|T]) -> fold(F, F(H,Start),T).
+
+
+reverse(L) ->
+    fold(fun(X,Acc) -> [X|Acc] end, [], L).
+
+
+map2(F,L) ->
+    reverse(fold(fun(X,Acc) -> [F(X)|Acc] end, [], L)).
+
+
+filter2(Pred, L) ->
+    F= fun(X, Acc) ->
+            case Pred(X) of
+                true    -> [X|Acc];
+                false   -> Acc
+            end
+       end,
+    reverse(fold(F, [], L)).
 
 
