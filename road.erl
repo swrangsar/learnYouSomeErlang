338f1583348998618aa16ec42d6a1c0dd8c07eea
swrangsar basumatary <swrangsar@example.com>
2014-07-02 09:37:25 +0530

    commit at 20140702093725

--- road.erl
+++ road.erl
@@ -2,10 +2,11 @@
 -compile(export_all).
 
 
-main() ->
-    File = "road.txt",
-    {ok, Bin} = file:read_file(File),
-    parse_map(Bin).
+main([FileName]) ->
+    {ok, Bin} = file:read_file(FileName),
+    Map = parse_map(Bin),
+    io:format("~p~n",[optimal_path(Map)]),
+    erlang:halt(0).
 
 
 parse_map(Bin) when is_binary(Bin) ->
@@ -24,5 +25,14 @@ group_vals([A,B,X|Rest], Acc) ->
 shortest_step({A,B,X}, {{DistA,PathA}, {DistB,PathB}}) ->
     OptA1 = {DistA + A, [{a,A}|PathA]},
     OptA2 = {DistB + B + X, [{x,X}, {b,B}|PathB]},
-    OptB1 = {DistB + B, [{b,B|PathB}]},
-    
+    OptB1 = {DistB + B, [{b,B}|PathB]},
+    OptB2 = {DistA + A + X, [{x,X}, {a,A}|PathA]},
+    {erlang:min(OptA1,OptA2), erlang:min(OptB1, OptB2)}.
+
+
+optimal_path(Map) ->
+    {A,B} = lists:foldl(fun shortest_step/2, {{0,[]}, {0,[]}}, Map),
+    {_Dist,Path} = if hd(element(2,A)) =/= {x,0} -> A;
+                      hd(element(2,B)) =/= {x,0} -> B
+                   end,
+    lists:reverse(Path).
