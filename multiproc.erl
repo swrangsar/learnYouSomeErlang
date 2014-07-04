c8d4916c3d9071e8cf0a7032ff8bbd5e2109f851
swrangsar basumatary <swrangsar@example.com>
2014-07-04 10:55:39 +0530

    commit at 20140704105538

--- multiproc.erl
+++ multiproc.erl
@@ -14,3 +14,21 @@ flush() ->
     after 0 ->
         ok
     end.
+
+
+important() ->
+    receive
+        {Priority, Message} when Priority > 10 ->
+            [Message | important()]
+    after 0 ->
+        normal()
+    end.
+
+
+normal() ->
+    receive
+        {_, Message} ->
+            [Message | normal()]
+    after 0 ->
+        []
+    end.
